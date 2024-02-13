2d8c31f089f590307d8f500d4a19e0a0f12ef100
Nabiyevich21 <mmedia10020@gmail.com>
2024-02-13 21:10:31 +0500

    dark mode added

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-moon");
+  if (this.classList.toggle("bi-brightness-high-fill")) {
+    body.style.background = "white";
+    body.style.color = "rgb(41, 41, 41)";
+    body.style.transition = "1s";
+  } else {
+    body.style.background = "rgb(41, 41, 41)";
+    body.style.color = "white";
+    body.style.transition = "1s";
+  }
+});
